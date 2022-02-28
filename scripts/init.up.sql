4c48e5405d67279e553cb6d9124f7ce1bce326cc
Naumov Andrei <naumov_a_a@outlook.com>
2022-03-01 02:42:52 +0300

    change openapi and README according to db schema changes

--- scripts/init.up.sql
+++ scripts/init.up.sql
@@ -1,26 +1,12 @@
 CREATE TABLE accounts (
-    user_id INTEGER,
-    balance NUMERIC NOT NULL DEFAULT 0.00,
-    PRIMARY KEY (user_id),
-    CONSTRAINT not_negative_balance CHECK (balance >= 0.00)
+    user_id INTEGER PRIMARY KEY,
+    balance NUMERIC NOT NULL DEFAULT 0.00 CHECK (balance >= 0.00)
 );
 
 CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    debit_user_id INTEGER,
-    credit_user_id INTEGER,
+    date TIMESTAMP NOT NULL,
+    account_id INTEGER REFERENCES accounts ON DELETE CASCADE,
     amount NUMERIC NOT NULL,
-    description VARCHAR(255),
-    CONSTRAINT fk_debit_user
-        FOREIGN KEY (debit_user_id)
-            REFERENCES accounts(user_id)
-            ON DELETE SET NULL,
-    CONSTRAINT fk_credit_user
-        FOREIGN KEY (credit_user_id)
-            REFERENCES accounts(user_id)
-            ON DELETE SET NULL,
-    CONSTRAINT debit_or_credit
-        CHECK (debit_user_id IS NOT NULL OR credit_user_id IS NOT NULL),
-    CONSTRAINT positive_amount CHECK (amount > 0.00)
+    description VARCHAR(255)
 );
